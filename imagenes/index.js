c540c79dccd11787cdbdbd81ee649ece4b32c8a6
Robert Romero <enanow41romero@gmail.com>
2019-09-09 18:28:09 -0400

    Arreglando imagenes

---
+++ imagenes/index.js
@@ -0,0 +1,9 @@
+function mostrar() {
+   var x = document.getElementById('alerta');
+   if (x.style.display === 'none') {
+       x.style.display = 'block';
+   } else {
+       x.style.display = 'none';
+   }
+}
+

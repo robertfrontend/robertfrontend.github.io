f02873f570552e908f8e6a1f880f2285291f5db7
unknown <enanow41romero@gmail.com>
2020-01-03 19:18:50 -0400

    nuevo front jeje de mi primera pagina lol

--- imagenes/index.js
+++
@@ -1,9 +0,0 @@
-function mostrar() {
-   var x = document.getElementById('alerta');
-   if (x.style.display === 'none') {
-       x.style.display = 'block';
-   } else {
-       x.style.display = 'none';
-   }
-}
-

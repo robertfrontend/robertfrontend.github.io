3d3b61c5a5fc998f0ca37a51acd8196b4f1e5c50
Robert Romero Medina <enanow41romero@gmail.com>
2020-06-23 08:22:21 -0400

    agregando mi cv

--- Js/script.js
+++ Js/script.js
@@ -75,7 +75,8 @@ var mensajeHTML = `
 
 // funcion validar formulario
 botonValidar.addEventListener('click', () => {
-    if(nombre === '' || correo === ''){
+    let valid = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    if(nombre === '' || correo === '' && correo === valid){
         alerta.innerHTML = mensajeHTML
     }
     else {
